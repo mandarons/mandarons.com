ca45a13560acdb50e3dcdfee9b2c018eec73deac
Mandar Patil <mandarons@pm.me>
2021-02-25 09:34:31 -0800

    Update cibuild.sh

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -2,5 +2,3 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-aws s3 sync public/ s3://mandarons.com --acl public-read --delete
-aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_DISTRIBUTION_ID} --paths "/*"
