94983ec5bdc6d95e73c8630378f6ef902414ab01
Mandar Patil <mandarons@pm.me>
2021-02-25 17:57:56 -0800

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,16 +8,16 @@ current_branch=$(git branch --show-current)
 echo "Current branch: ${current_branch}"
 rm -rf public
 bundle exec jekyll build &&
-    git checkout gh-pages &&
-    cd public &&
+    # git checkout gh-pages &&
+    # cd public &&
     git add . &&
     git commit -m "$1" &&
     # git push origin gh-pages &&
-    cd .. &&
-    echo "Ready to deploy to gh-pages."
-# echo "Successfully deployed to GitHub."
-if [ ${current_branch} != "gh-pages" ]; then
-    echo "Checking out previous branch ..." &&
-        git checkout ${current_branch}
-fi
-echo "All good."
+    # cd .. &&
+    # echo "Ready to deploy."
+    # echo "Successfully deployed to GitHub."
+    # if [ ${current_branch} != "gh-pages" ]; then
+    #     echo "Checking out previous branch ..." &&
+    #         git checkout ${current_branch}
+    # fi
+    echo "All good."
